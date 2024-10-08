d3432ad37a32a2e707735045476ae13086bea87a
Javier Gamboa <jgamboa19@alumnos.utalca.cl>
2024-10-07 23:44:47 -0300

    add paisaje.jpg

--- FRONTEND/src/sections/Carousel.vue
+++ FRONTEND/src/sections/Carousel.vue
@@ -4,7 +4,7 @@ export default {
     return {
       activeIndex: 0,
       images: [
-        "/images/ciervo_en_paisaje.jpg",
+        "/images/paisaje.jpg",
         "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
         "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
         "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
