4f237ee0472e3b3bb629be2f9528a244fd93cecb
Javier Gamboa <jgamboa19@alumnos.utalca.cl>
2024-10-10 18:16:58 -0300

    add JWT (acces_token) can still change

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,11 +1,30 @@
 import express from 'express';
 import { PORT } from './config.js';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
 app.use(express.json()); //middleware
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+    const token = req.cookies.acces_token;
+    req.session = { user:null }
+
+    try{
+        const data = jwt.verify(token, SECRET_JWT_KET);
+        req.session.user = data;
+    } catch{}
+
+    next(); //SEGUIR A LA SIGUIENTE RUTA O MIDDELWARE
+})
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    const token = req.cookies.acces_token;
+    
+    const { user } = req.session;
+    res.render('index', user);
 });
 
 app.listen(PORT, () => {
@@ -14,11 +33,27 @@ app.listen(PORT, () => {
 
 
 
+
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
+
+
     try {
         const user = await UserRepository.login({ username, password });
-        res.send({ user });
+        const token = jwt.sign(
+            { id:user._id, username: user.username }, 
+            SECRET_JWT_KET,
+            { 
+                expiresIn: '1h' 
+            });
+        res
+        .cookie('acces_token', token, { 
+            httpOnly: true,  //la cookie solo se puede acceder en el servidor
+            secure: process.env.NODE_ENV === 'production', //solo se puede acceder en https OJO CON ESTO PORQUE SI TRABAJAMOS CON HTTP DUDO QUE FUNCIONE
+            sameSite: 'strict', // la cookie solo se puede acceder en el mismo dominio
+            maxAge: 3600000 //1 hora
+         }) 
+        .send({ user, token }); //NO DEVOLVER EL TOKEN A LOCALSTORAGE NI SESSION STORAGE, SIEMPRE ES MEJOR LA COOKIE 
     } catch (error) {
         res.status(401).send({ error: error.message });
     }
@@ -28,9 +63,13 @@ app.post('/login', async (req, res) => {
 
 
 
-app.post('/logout', (req, res) => {});
+app.post('/logout', (req, res) => {
+    res
+        .clearCookie('acces_token') 
+        .json({ message: 'Logout succesful' });
+});
 
-/* Esto puede quitarse */
+
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
     console.log({username, password})
@@ -48,10 +87,8 @@ app.post('/register', async (req, res) => {
 
 
 app.get('/protected', (req, res) => {
-    //TODO: if sesion del usuario
-    res.render('protected', {username: 'user'}); //CAMBIAR POR EL USERNAME
-    //TODO: else 401
-
-
+    const { user } = req.session;
+    if(!user) return res.status(403).send({error: 'unauthorized'});
+    res.render('protected', user); // {_id, username }
 });
 
