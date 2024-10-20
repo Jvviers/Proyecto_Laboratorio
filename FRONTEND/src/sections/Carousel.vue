69c2d0262f7fccd9fa35b96966870bdb48fcd72a
Nico <ncuadra19@alumnos.utalca.cl>
2024-10-19 22:45:50 -0300

    Interfaz Responsive, estilos, y primeros avances en subida de archivos

--- FRONTEND/src/sections/Carousel.vue
+++ FRONTEND/src/sections/Carousel.vue
@@ -4,15 +4,15 @@ export default {
     return {
       activeIndex: 0,
       images: [
-        "/images/paisaje.jpg",
-        "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-        "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-        "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-        "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        { url: "/images/paisaje.jpg", alt: "paisaje" },
+        { url: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80", alt: "rostro 1" },
+        { url: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80", alt: "rostro 2" },
+        { url: "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80", alt: "rostro 3" },
+        { url: "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80", alt: "rostro 4" },
+        { url: "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80", alt: "rostro 5" },
       ],
-      isDragging: false, 
-      startX: 0, 
+      isDragging: false,
+      startX: 0,
     };
   },
   methods: {
@@ -28,117 +28,75 @@ export default {
     },
     startDrag(event) {
       this.isDragging = true;
-      this.startX = event.clientX; 
+      this.startX = event.clientX;
     },
     onDrag(event) {
       if (!this.isDragging) return;
 
-      const deltaX = event.clientX - this.startX; 
+      const deltaX = event.clientX - this.startX;
 
       if (deltaX > 50) {
         // Si arrastra a la derecha
         this.prevSlide();
-        this.isDragging = false; 
+        this.isDragging = false;
       } else if (deltaX < -50) {
-      
+
         this.nextSlide();
-        this.isDragging = false; 
+        this.isDragging = false;
       }
     },
     endDrag() {
-      this.isDragging = false; 
+      this.isDragging = false;
     },
   },
 };
 </script>
 
 <template>
-  <div
-    class="relative w-full"
-    @mousedown="startDrag"
-    @mousemove="onDrag"
-    @mouseup="endDrag"
-    @mouseleave="endDrag"
-  >
+  <div class="relative w-full" @mousedown="startDrag" @mousemove="onDrag" @mouseup="endDrag" @mouseleave="endDrag">
     <!-- Carousel wrapper -->
-    <div class="relative h-[80vh] overflow-hidden">
+    <div class="relative h-[80vh] overflow-hidden -z-20">
       <!-- Carrusel de items -->
-      <div
-        v-for="(image, index) in images"
-        :key="index"
+      <div v-for="(image, index) in images" :key="index"
         class="absolute inset-0 transition-opacity duration-1000 ease-in-out"
-        :class="{ 'opacity-100': activeIndex === index, 'opacity-0': activeIndex !== index }"
-      >
-        <img
-          :src="image"
-          class="block w-full h-full object-cover"
-          alt="Carousel Image"
-          draggable="false"
-        />
+        :class="{ 'opacity-100': activeIndex === index, 'opacity-0': activeIndex !== index }">
+        <img :src=image.url :alt=image.alt class="block w-full h-full object-cover" draggable="false" />
       </div>
     </div>
     <!-- Indicadores -->
     <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-      <button
-        v-for="(image, index) in images"
-        :key="index"
-        @click="goToSlide(index)"
-        class="w-3 h-3 rounded-full"
+      <button v-for="(image, index) in images" :key="index" @click="goToSlide(index)" class="w-3 p-0 h-3 rounded-full hover:bg-utal transitions"
         :class="{
-          'bg-blue-500': activeIndex === index,
-          'bg-gray-300': activeIndex !== index,
-        }"
-        aria-label="Slide"
-      ></button>
+          'bg-utal': activeIndex === index,
+          'bg-gray-200': activeIndex !== index,
+        }" aria-label="Slide"></button>
     </div>
     <!-- Controles del slider -->
-    <button
-      type="button"
-      class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-      @click="prevSlide"
-    >
-      <span
-        class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 focus:ring-4 focus:ring-white focus:outline-none"
-      >
-        <svg
-          class="w-4 h-4 text-white"
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+    <button type="button" class="px-4 absolute top-0 left-0 z-10 h-full outline-none bg-transparent hover:bg-transparent transitions"
+      @click="prevSlide">
+      <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 transitions">
+        <svg class="w-4 h-4 text-white" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+          stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path d="M15 19l-7-7 7-7"></path>
         </svg>
-        <span class="sr-only">Previous</span>
       </span>
     </button>
-    <button
-      type="button"
-      class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-      @click="nextSlide"
-    >
-      <span
-        class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 focus:ring-4 focus:ring-white focus:outline-none"
-      >
-        <svg
-          class="w-4 h-4 text-white"
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+    <button type="button" class="px-4 absolute top-0 right-0 z-10 h-full outline-none bg-transparent hover:bg-transparent transitions"
+      @click="nextSlide">
+      <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 transitions">
+        <svg class="w-4 h-4 text-white" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+          stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path d="M9 5l7 7-7 7"></path>
         </svg>
-        <span class="sr-only">Next</span>
       </span>
     </button>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+button:hover {
+  span {
+    background-color: #00cdcd;
+  }
+}
+</style>
