c1573bf3302336396c2a19fead0d825319fb1c9e
Nico <ncuadra19@alumnos.utalca.cl>
2024-10-26 22:00:45 -0300

    tamaño para tablas nuevas

--- BACKEND/squema/script.sql
+++ BACKEND/squema/script.sql
@@ -37,17 +37,17 @@ CREATE TABLE equipo(
 -- MANTENEDORES
 CREATE TABLE nombreequipos(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre text UNIQUE NOT NULL
+    nombre VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE tipomaterial(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre text UNIQUE NOT NULL
+    nombre VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE tipoproyecto(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre text UNIQUE NOT NULL
+    nombre VARCHAR(100) UNIQUE NOT NULL
 );
 
 INSERT INTO nombreequipos (nombre) VALUES ('Impresora Creality FDM');
